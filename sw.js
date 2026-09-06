095718ad653e105e1af2649c1fe0d804580babc8
Giovanne Ferrari <vanneferrarii@hotmail.com>
2026-09-06 10:19:55 -0300

    Adiciona icones profissionais aos indicadores

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='rito-shell-v13';
-const APP_SHELL=['./','./index.html','./styles.css?v=dashboard-ui-13','./app.js?v=dashboard-ui-13','./manifest.webmanifest?v=3','./assets/rito-logo-transparent.png?v=1','./assets/rito-monograma.png','./assets/icons/icon-192.png?v=3','./assets/icons/icon-512.png?v=3','./assets/icons/apple-touch-icon.png?v=3'];
+const CACHE_NAME='rito-shell-v14';
+const APP_SHELL=['./','./index.html','./styles.css?v=metric-icons-14','./app.js?v=metric-icons-14','./manifest.webmanifest?v=3','./assets/rito-logo-transparent.png?v=1','./assets/rito-monograma.png','./assets/icons/icon-192.png?v=3','./assets/icons/icon-512.png?v=3','./assets/icons/apple-touch-icon.png?v=3'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));

2589469bc9ff36be669e5a77a1d9d63b51e87aab
Giovanne Ferrari <vanneferrarii@hotmail.com>
2026-09-08 08:30:41 -0300

    Adiciona status e duplicacao de compromissos

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='rito-shell-v46';
-const APP_SHELL=['./','./index.html','./styles.css?v=event-cycle-45','./app.js?v=event-cycle-45','./manifest.webmanifest?v=5','./assets/rito-logo-transparent.png?v=1','./assets/rito-monograma.png','./assets/icons/favicon-32.png?v=5','./assets/icons/favicon-64.png?v=5','./assets/icons/icon-192.png?v=5','./assets/icons/icon-512.png?v=5','./assets/icons/apple-touch-icon.png?v=5'];
+const CACHE_NAME='rito-shell-v47';
+const APP_SHELL=['./','./index.html','./styles.css?v=event-cycle-46','./app.js?v=event-cycle-46','./manifest.webmanifest?v=5','./assets/rito-logo-transparent.png?v=1','./assets/rito-monograma.png','./assets/icons/favicon-32.png?v=5','./assets/icons/favicon-64.png?v=5','./assets/icons/icon-192.png?v=5','./assets/icons/icon-512.png?v=5','./assets/icons/apple-touch-icon.png?v=5'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
